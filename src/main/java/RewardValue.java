afc610201547e842c2d5bccc0fc3c4f43b2cc1bb
marcie14 <marcielle.legarde@utexas.edu>
2024-01-19 14:44:48 -0600

    reworked with jpmc example - to continue to unit testing

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,24 +1,30 @@
 public class RewardValue {
-    private double cashValue;
-    private int milesValue;
+    private final double cashValue;
+    public static final double CONVERSION_RATE = 0.0035;
 
     // constructors
     public RewardValue(double cashValue) { // convert cash to miles
         this.cashValue = cashValue;
-        this.milesValue = (int) (cashValue / 0.0035);
     }
 
     public RewardValue(int milesValue){ // convert miles to cash
-        this.milesValue = milesValue;
-        this.cashValue = (int) (milesValue / 0.0035);
+        this.cashValue = convertToCash(milesValue);
     }
 
     // methods
+    private static int convertToMiles(double cashValue){
+        return (int) (cashValue / CONVERSION_RATE);
+    }
+
+    private static double convertToCash(int milesValue){
+        return milesValue * CONVERSION_RATE;
+    }
+
     public double getCashValue(){
         return cashValue;
     }
-    public double getMilesValue(){
-        return milesValue;
-    }
 
+    public int getMilesValue(){
+        return convertToMiles(this.cashValue);
+    }
 }
